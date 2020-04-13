432b34c1ee23e2dc6f7d45fc1859f170fb74360b
InvictusFire <tech.invictus@github.com>
2020-04-13 20:30:29 -0300

    Mais dois exercicios

---
+++ 100NumerosSomados.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+main(){
+	int c, soma = 0;
+	for(c = 0; c < 4; c ++){
+		soma += c;
+		printf("%i\n", soma);
+	}
+}
---
+++ verifParImpar.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main(){
+	int conta;
+	
+	for(int c = 0; c < 100; c ++){
+		conta = c % 2;
+		
+		if(conta == 0){
+			printf("%i\n", c);
+		}
+	}
+}
